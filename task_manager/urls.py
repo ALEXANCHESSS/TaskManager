ebf8ac2fe6b639daa8d5985af8a374fb53549bd9
Aleksandr Kuznetsov <aleksandr.kuznetsov@dualbootpartners.com>
2024-07-14 21:26:07 +0200

    connect JWT

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,6 +21,10 @@ from main.admin import task_manager_admin_site
 from main.views import StatusViewSet, TagViewSet, TaskViewSet, UserViewSet
 from rest_framework import routers
 from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -52,6 +56,8 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("admin/", task_manager_admin_site.urls),
     path("api/", include(router.urls)),
 ]
